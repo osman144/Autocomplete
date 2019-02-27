98af6893471371d7981988055378e377b1e82117
Salah Osman <osman144@umn.edu>
2019-02-26 22:43:42 -0600

    updated conditional logic in autcomplete component, added component and imported on appjs

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -5,6 +5,8 @@ export default class AutoCompleteText extends React.Component {
     // Ensure component set up correctly
     constructor(props){
         super(props)
+
+        // Based on list, render markup
         this.items = [
             'Alabama', 
             'Alaska', 
@@ -57,9 +59,41 @@ export default class AutoCompleteText extends React.Component {
             'Wisconsin', 
             'Wyoming',
         ];
+        this.state = {
+            suggestions: [],
+        }
     }
 
+    // State changes recognized, changes what gets renders, makes it dynamic
+    onTextChange = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if(value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i')
+        }
+        if(value.length === 0){
+            this.setState(() => ({
+                suggestions: [],
+            }))
+        } else {
+            const suggestions = this.items.sort().filter(v => v.test(regex));
 
+            // Update state with filtered list of suggestion
+            this.setState(() => ({ suggestions }));
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <input onChange={onTextChange} type="text" />
+                <ul>
+                    {/* Show all items in list */}
+                    {this.items.map((item) => <li>{item}</li>)}
+                </ul>
+            </div>
+        )
+    }
 
 
 } 
